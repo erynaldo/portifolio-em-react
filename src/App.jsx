6b082015381bdda13da49f09b28a69953ab2eaa8
erynaldo <erynaldo@hotmail.com>
2026-04-13 23:58:17 -0300

    mais uma atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -13,36 +13,42 @@ const meusProjetos = [
     id: 1,
     titulo: "Projeto 01 - Sistema Anti-Bullying",
     descricao: "Esse sistema tem como objetivo receber denúncias de bullying ocorridos no ambiente escolar.",
+    status: "Ajustes",
     link: "https://github.com/erynaldo/projeto-bullying"
   },
   {
     id: 2,
     titulo: "Projeto 02 - Sistema Espaço do Aluno",
     descricao: "Sistema de login para que o aluno possa acessar seus materiais, apostilas, atividades, ver as notas.",
+    status: "Finalizado",
     link: "https://github.com/erynaldo/ceti.sjb"
   },
   {
     id: 3,
     titulo: "Projeto 03 - Sistema para Prefeituras",
     descricao: "Sistema para que as Prefeituras possam oferecer serviços aos usuários, como disponibilizar contra-cheques aos servidores municipais, boletos de IPTU ou outros documentos aos cidadãos.",
+    status: "Iniciado",
     link: "https://github.com/erynaldo/meu-app-react"
   },
   {
     id: 4,
     titulo: "Projeto 04 - Plataforma de Aluguel de Filmes",
     descricao: "Sistema foi desenvovido para ser uma plataforma de aluguel de filmes, não precisa de assinatura, o cliente aluga um filme e ele fica disponível por alguns dias para assistir.",
+    status: "Finalizado",
     link: "https://github.com/erynaldo/projet-plataforma-filmes"
   },
   {
     id: 5,
     titulo: "Projeto 05 - Calculadora de IMC",
     descricao: "Sistema para Nutricionistas e / ou Profissionais da Saúde para calcular o IMC de um paciente.",
+    status: "Iniciado",
     link: "https://github.com/erynaldo/first-project-portifolio"
   },
   {
     id: 6,
     titulo: "Projeto 06 - Jogo da Velha",
     descricao: "Joguinho da Velha para passar o tempo.Desenvolvido em HTML, CSS e Javascript.",
+    status: "Ajustes",
     link: "https://github.com/erynaldo/projeto-jogo-da-velha"
   }
 ];
@@ -78,7 +84,7 @@ function App() {
         <section id="sobre">
           {/* <h2>Sobre Mim</h2> */}
           <img src={fotoPerfil} alt="Foto de Erinaldo Cardoso" />
-          <p><span id="sobre-mim">Olá, meu nome é Erinaldo Cardoso</span> <br /> Atualmente estou cursando Tecnologia em Sistemas para Internet pela UAPI/UESPI, modalidade semi-presencial e em breve se Deus quiser estarei formado. A paixão pela área de TI vem
+          <p><span id="titulo-sobre-mim">Sobre Mim</span><span id="sobre-mim">Olá, meu nome é Erinaldo Cardoso</span> <br /> Atualmente estou cursando Tecnologia em Sistemas para Internet pela UAPI/UESPI, modalidade semi-presencial e em breve se Deus quiser estarei formado. A paixão pela área de TI vem
             desde cedo, desde os meus 14 anos. Antes de iniciar esse curso eu já me identificava muito com essa área da
             programação, especialmente desenvolvimento web. Enfim, em 2024, surgiu a oportunidade de poder fazer esse curso e estou gostando muito, aproveitando ao máximo para aprender sobre esse mundo da programação.
             <br /><br /><a href="https://github.com/erynaldo" target="_blank" className="meu-link">Meu Github</a><a href="https://www.linkedin.com/in/erinaldo-cardoso-709bb31a3/" target="_blank" className="meu-link">Meu Linkedin</a>
@@ -93,6 +99,7 @@ function App() {
                 key={projeto.id}
                 titulo={projeto.titulo}
                 descricao={projeto.descricao}
+                status={projeto.status}
                 link={projeto.link}
               />
             ))}
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import fotoProjeto from '../assets/img-projeto.png';
 
-function ProjectCard({ titulo, descricao, link }) {
+function ProjectCard({ titulo, descricao, status, link }) {
     return (
         <a href={link} target="_blank" rel="noopener noreferrer" className="card-projeto">
             <article>
@@ -9,7 +9,8 @@ function ProjectCard({ titulo, descricao, link }) {
                 <img src={fotoProjeto} alt="Foto de Erinaldo Cardoso" className="img-projeto"/>
                 <h3>{titulo}</h3>
                 <p>{descricao}</p>
-                {/* <a href={link} target="_blank" rel="noopener noreferrer">Ver no GitHub</a> */}
+                <span className="status-projeto">{status}</span>
+                <p className="projeto-git">Ver o projeto no Github</p>
             </article>
         </a>
     );
